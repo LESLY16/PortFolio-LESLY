b926049fac3c6313da04eed07b5c9d10d57f2b6c
LESLY M DSOUZA <leslyd_35102@outlook.com>
2026-06-20 18:06:00 +0530

    Remove ResumeEmbed component from portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import CareerGap from './components/CareerGap'
-import ResumeEmbed from './components/ResumeEmbed'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 
@@ -15,7 +14,6 @@ export default function App() {
       <main>
         <section id="about"><About /></section>
         <section id="career-gap"><CareerGap /></section>
-        <section id="resume"><ResumeEmbed /></section>
         <section id="projects"><Projects /></section>
         <section id="contact"><Contact /></section>
       </main>
